6d199750fd2a198aecb12c0a989e7819e050df60
anastasija-grekova <125025406+anastasija-grekova@users.noreply.github.com>
2023-03-17 21:26:11 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,41 +1,103 @@
 # python3
 
+import os
+#indexMas = []
+swaps = []
 
 def build_heap(data):
-    swaps = []
+    #swaps = []
+    #count = 0
+    for elementIndex in range(len(data) - 1, -1, -1):
+        sort(data, elementIndex)
+    #rootIndex = data.index(min(data))
+    #middleIndex = (len(data) - 1)/2
+    #print(rootIndex)
+    #for i in enumerate(data):
+    #swaps.append(count)
+    
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
+    #return swaps
+
+def sort(data, elementIndex):
+    minEl = elementIndex 
+    leftEl = 2 * elementIndex + 1  
+    rightEl = 2 * elementIndex + 2 
+
+    if leftEl < len(data) and data[elementIndex] > data[leftEl]:
+        minEl = leftEl
 
 
-    return swaps
+    if rightEl < len(data) and data[minEl] > data[rightEl]:
+        minEl = rightEl
+
+    if minEl != elementIndex:
+        data[elementIndex],data[minEl] = data[minEl],data[elementIndex]
+        #count = count + 1
+        swaps.append([elementIndex, minEl])
+        #swaps.append(minEl)
+        sort(data, minEl)
 
 
 def main():
     
+    check = input()
+    check = check.replace("\r","")
+    check = check.replace("\n","")
+    if check == 'I':
+        n = int(input())
+        if n < 1 and n > 100000:
+            print("error")
+            quit()
+        data = list(map(int, input().split()))
+        assert len(data) == n
+        build_heap(data)
+
+    if check == 'F':
+        path = os.getcwd() + '/tests'
+        os.chdir(path)
+        file_name = input()
+        if 'a' in file_name:
+            print("error")
+            quit()        
+        else:
+            file_path = f"{path}/{file_name}"
+            with open(file_path, "r", encoding="utf-8-sig") as f:
+                newLines = f.readlines()
+                n = int(newLines[0].replace("\n", ""))
+                if n < 1 and n > 100000:
+                    print("error")
+                    quit()
+                data = newLines[1].replace("\n", "").split()
+                data = [int(i) for i in data] 
+                assert len(data) == n
+            build_heap(data)
+
+
     # TODO : add input and corresponding checks
     # add another input for I or F 
     # first two tests are from keyboard, third test is from a file
 
 
     # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
+    #n = int(input())
+    #data = list(map(int, input().split()))
 
     # checks if lenght of data is the same as the said lenght
-    assert len(data) == n
+    #assert len(data) == n
 
     # calls function to assess the data 
     # and give back all swaps
-    swaps = build_heap(data)
+    #build_heap(data)
 
     # TODO: output how many swaps were made, 
     # this number should be less than 4n (less than 4*len(data))
 
 
     # output all swaps
-    print(len(swaps))
+    print(len(swaps), end = ' ')
     for i, j in swaps:
-        print(i, j)
+        print(i, j, end = ' ')
 
 
 if __name__ == "__main__":
